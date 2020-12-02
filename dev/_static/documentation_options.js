345c892f7ffef815eec920846750337663456980
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-12-02 23:26:14 +0000

    Update docs after building Travis build 74 of ioos/gliderpy
    The docs were built from the branch 'master' against the commit
    efd670d5600371edc8202eef942190f7dba75476.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/ioos/gliderpy/jobs/452947964.
    
    The doctr command that was run is
    
        /home/travis/micromamba/envs/TEST/lib/python3.9/site-packages/doctr/__main__.py deploy --build-tags --key-path github_deploy_key_ioos_gliderpy.enc --built-docs docs/build/html dev

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev76+ga8a3c9d',
+    VERSION: '0.1.dev85+gefd670d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
