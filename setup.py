23e4f5887c65969b22b6ec6ec9f436fbe97a9ed9
Filipe Fernandes <ocefpaf@gmail.com>
2023-04-11 14:21:23 -0300

    remove

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    use_scm_version={
-        "write_to": "gliderpy/_version.py",
-        "write_to_template": '__version__ = "{version}"',
-        "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
-    }
-)
