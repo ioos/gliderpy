6adfaff5385f82ff3c3bf9f4882b469470d64ce9
Filipe Fernandes <ocefpaf@gmail.com>
2023-10-13 10:46:12 -0300

    add profile_id

--- gliderpy/servers.py
+++ gliderpy/servers.py
@@ -6,27 +6,28 @@ Server names and aliases that point to an ERDDAP instance
 
 server_vars = {
     "https://gliders.ioos.us/erddap": [
-        "pressure",
         "latitude",
         "longitude",
+        "pressure",
+        "profile_id",
         "salinity",
         "temperature",
         "time",
-        "profile_id",
     ],
 }
 
 server_parameter_rename = {
+    "ctdgv_m_glider_instrument_practical_salinity (1)": "salinity",
+    "ctdgv_m_glider_instrument_sci_water_pressure_dbar (dbar)": "pressure",
+    "ctdgv_m_glider_instrument_sci_water_temp (deg_c)": "temperature",
+    "dataset_url": "dataset_url",
     "latitude (degrees_north)": "latitude",
     "longitude (degrees_east)": "longitude",
-    "salinity (1)": "salinity",
-    "psal (psu)": "salinity",
-    "ctdgv_m_glider_instrument_practical_salinity (1)": "salinity",
-    "temperature (celsius)": "temperature",
-    "temp (degree_celsius)": "temperature",
-    "ctdgv_m_glider_instrument_sci_water_temp (deg_c)": "temperature",
     "pres (decibar)": "pressure",
     "pressure (dbar)": "pressure",
-    "ctdgv_m_glider_instrument_sci_water_pressure_dbar (dbar)": "pressure",
-    "dataset_url": "dataset_url",
+    "profile_id": "profile_id",
+    "psal (psu)": "salinity",
+    "salinity (1)": "salinity",
+    "temp (degree_celsius)": "temperature",
+    "temperature (celsius)": "temperature",
 }
