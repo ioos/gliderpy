043e10c2703a152f112ac57515e110b7b4a5475f
celestinelindarto <celestinelindarto@gmail.com>
2024-02-28 17:27:38 +0700

    Added a testing method for the data type of the glider df

--- tests/test_fetchers.py
+++ tests/test_fetchers.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 
 from gliderpy.fetchers import GliderDataFetcher
 from gliderpy.servers import server_parameter_rename
@@ -32,3 +33,31 @@ def test_standardise_variables_ioos():
     variables = df.columns
     for var in variables:
         assert var in server_parameter_rename.values()
+
+# testing the data type for each column
+# this was adapted from How to Validate Your DataFrames with Pytest by Data Products
+# https://plainenglish.io/blog/how-to-validate-your-dataframes-with-pytest-b238d2891d12
+@pytest.fixture
+def glider_data_type() -> dict[str, np.dtype]:
+    """
+    Get the data type of glider df
+    :return: a dict of column name and its respective dtype 
+    """
+    glider_grab = GliderDataFetcher()
+    glider_grab.fetcher.dataset_id = "whoi_406-20160902T1700"
+    df = glider_grab.to_pandas()
+    glider_dtype_dict = df.dtypes.to_dict()
+    return glider_dtype_dict
+
+def test_glider_data_type(glider_data_type: callable):
+    """
+    Test the data type of glider data against the schema data type
+    """
+    glider_data_schema = {'latitude': np.dtype('float64'),
+                        'longitude': np.dtype('float64'),
+                        'pressure': np.dtype('float64'),
+                        'profile_id': np.dtype('int64'),
+                        'salinity': np.dtype('float64'),
+                        'temperature': np.dtype('float64'),
+                        'dataset_url': np.dtype('O')}
+    assert glider_data_type == glider_data_schema
