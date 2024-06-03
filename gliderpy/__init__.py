45aef0ab6ab6b199015610821bf6a0c7ce8174cc
FloraSauerbronn <flora.msauerbronn@gmail.com>
2024-06-03 17:29:12 -0300

    Update init with imports and __all__ = [..]

--- gliderpy/__init__.py
+++ gliderpy/__init__.py
@@ -4,3 +4,12 @@ try:
     from ._version import __version__
 except ImportError:
     __version__ = "unknown"
+
+
+from .plotting import plot_transect
+from .fetchers import GliderDataFetcher
+
+__all__ = [
+    "GliderDataFetcher",
+    "plot_transect"
+]
