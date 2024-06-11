1e397cb67998aeb7193f7140be54de06eaba3dff
FloraSauerbronn <flora.msauerbronn@gmail.com>
2024-06-11 14:27:17 -0300

    Updating __init__.py with plot_track

--- gliderpy/__init__.py
+++ gliderpy/__init__.py
@@ -5,11 +5,13 @@ try:
 except ImportError:
     __version__ = "unknown"
 
+from .plotting import plot_track
 from .plotting import plot_transect
 from .fetchers import GliderDataFetcher
 
 
 __all__ = [
     "GliderDataFetcher",
+    "plot_track",
     "plot_transect",
 ]
