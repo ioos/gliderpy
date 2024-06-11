4772432709a95da21078930794b9ec42c434b32c
FloraSauerbronn <flora.msauerbronn@gmail.com>
2024-06-11 14:36:44 -0300

    Undoing changes for test failure

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -15,7 +15,7 @@ def test_plot_transect():
     glider_grab.fetcher.dataset_id = "whoi_406-20160902T1700"
     df = glider_grab.to_pandas()
     # Generate the plot
-    fig, ax = plot_transect(df, 'salinity')
+    fig, ax = plot_transect(df, 'temperature')
 
     # Return the figure for pytest-mpl to compare
     return fig
