b731bb7e87c8870a2abd099a89f9fe8c12aa8017
FloraSauerbronn <flora.msauerbronn@gmail.com>
2024-06-11 14:13:28 -0300

    Applying corrections to test_plotty.py

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,19 +1,21 @@
 """Test transect."""
 
 import pytest
-import matplotlib.pyplot as plt
-
+import matplotlib as mpl
+from pathlib import Path
 from gliderpy.plotting import plot_transect
 from gliderpy.fetchers import GliderDataFetcher
 
-@pytest.mark.mpl_image_compare
+root = Path(__file__).parent
+
+@pytest.mark.mpl_image_compare(baseline_dir=root.joinpath("baseline/"))
 def test_plot_transect():
     glider_grab = GliderDataFetcher()
 
     glider_grab.fetcher.dataset_id = "whoi_406-20160902T1700"
     df = glider_grab.to_pandas()
     # Generate the plot
-    fig, ax = plot_transect(df, 'temperature')
+    fig, ax = plot_transect(df, 'salinity')
 
     # Return the figure for pytest-mpl to compare
     return fig
