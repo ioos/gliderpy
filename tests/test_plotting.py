d2526f071fba96c81d3f6d95966e8ffe040bde9d
FloraSauerbronn <flora.msauerbronn@gmail.com>
2024-06-14 15:49:07 -0300

    Add new function test_plot_ctd

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -5,6 +5,7 @@ import matplotlib as mpl
 from pathlib import Path
 from gliderpy.plotting import plot_track
 from gliderpy.plotting import plot_transect
+from gliderpy.plotting import plot_ctd
 from gliderpy.fetchers import GliderDataFetcher
 
 root = Path(__file__).parent
@@ -31,6 +32,19 @@ def test_plot_transect():
     df = glider_grab.to_pandas()
     # Generate the plot
     fig, ax = plot_transect(df, 'temperature')
+
+    # Return the figure for pytest-mpl to compare
+    return fig
+
+
+@pytest.mark.mpl_image_compare(baseline_dir=root.joinpath("baseline/"))
+def test_plot_ctd():
+    glider_grab = GliderDataFetcher()
+
+    glider_grab.fetcher.dataset_id = "whoi_406-20160902T1700"
+    df = glider_grab.to_pandas()
+    # Generate the plot
+    fig, ax = plot_ctd(df, 'temperature')
 
     # Return the figure for pytest-mpl to compare
     return fig
