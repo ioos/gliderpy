e6529a1ab512814ccc50a73f9ba95a6af890d208
FloraSauerbronn <flora.msauerbronn@gmail.com>
2024-06-18 16:45:08 -0300

    Adding new test function for multiple figures in plot_transect

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,7 +1,8 @@
 """Test transect."""
 
+import cmcrameri
 import pytest
-import matplotlib as mpl
+import matplotlib.pyplot as plt
 from pathlib import Path
 from gliderpy.plotting import plot_track
 from gliderpy.plotting import plot_transect
@@ -31,6 +32,21 @@ def test_plot_transect():
     df = glider_grab.to_pandas()
     # Generate the plot
     fig, ax = plot_transect(df, 'temperature')
+
+    # Return the figure for pytest-mpl to compare
+    return fig
+
+@pytest.mark.mpl_image_compare(baseline_dir=root.joinpath("baseline/"))
+def test_plot_transect_multiple_figures():
+    glider_grab = GliderDataFetcher()
+
+    glider_grab.fetcher.dataset_id = "whoi_406-20160902T1700"
+    df = glider_grab.to_pandas()
+
+    # Generate the plot
+    fig, (ax0, ax1) = plt.subplots(figsize=(15, 9), nrows=2, sharex=True,sharey=True)    
+    df.plot_transect(var="temperature", ax=ax0)  
+    df.plot_transect(var="salinity", ax=ax1, cmap=cmcrameri.cm.davos)
 
     # Return the figure for pytest-mpl to compare
     return fig
