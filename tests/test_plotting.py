5f2353d5a0538d241d43ab74f23fb83c846c2b78
FloraSauerbronn <flora.msauerbronn@gmail.com>
2024-06-19 09:57:27 -0300

    Enhance plot_transect tests: add multiple axes and image size specification

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,52 +1,44 @@
-"""Test transect."""
-
 import cmcrameri
 import pytest
 import matplotlib.pyplot as plt
 from pathlib import Path
-from gliderpy.plotting import plot_track
-from gliderpy.plotting import plot_transect
+from gliderpy.plotting import plot_track, plot_transect
 from gliderpy.fetchers import GliderDataFetcher
 
 root = Path(__file__).parent
 
-@pytest.mark.mpl_image_compare(baseline_dir=root.joinpath("baseline/"))
-def test_plot_track():
+@pytest.fixture
+def glider_data():
     glider_grab = GliderDataFetcher()
-
     glider_grab.fetcher.dataset_id = "whoi_406-20160902T1700"
     df = glider_grab.to_pandas()
-    # Generate the plot
-    fig, ax = plot_track(df)
-
-    # Return the figure for pytest-mpl to compare
-    return fig
-
-
+    return df
 
 @pytest.mark.mpl_image_compare(baseline_dir=root.joinpath("baseline/"))
-def test_plot_transect():
-    glider_grab = GliderDataFetcher()
-
-    glider_grab.fetcher.dataset_id = "whoi_406-20160902T1700"
-    df = glider_grab.to_pandas()
+def test_plot_track(glider_data):
     # Generate the plot
-    fig, ax = plot_transect(df, 'temperature')
-
+    fig, ax = plot_track(glider_data)
     # Return the figure for pytest-mpl to compare
     return fig
 
 @pytest.mark.mpl_image_compare(baseline_dir=root.joinpath("baseline/"))
-def test_plot_transect_multiple_figures():
-    glider_grab = GliderDataFetcher()
-
-    glider_grab.fetcher.dataset_id = "whoi_406-20160902T1700"
-    df = glider_grab.to_pandas()
-
+def test_plot_transect(glider_data):
     # Generate the plot
-    fig, (ax0, ax1) = plt.subplots(figsize=(15, 9), nrows=2, sharex=True,sharey=True)    
-    df.plot_transect(var="temperature", ax=ax0)  
-    df.plot_transect(var="salinity", ax=ax1, cmap=cmcrameri.cm.davos)
-
+    fig, ax = plot_transect(glider_data, 'temperature')
     # Return the figure for pytest-mpl to compare
     return fig
+
+@pytest.mark.mpl_image_compare(baseline_dir=root.joinpath("baseline/"))
+def test_plot_transect_multiple_figures(glider_data):
+    # Generate the plot with multiple figures
+    fig, (ax0, ax1) = plt.subplots(figsize=(15, 9), nrows=2, sharex=True, sharey=True)
+    glider_data.plot_transect(var="temperature", ax=ax0)
+    glider_data.plot_transect(var="salinity", ax=ax1, cmap=cmcrameri.cm.davos)
+    # Return the figure for pytest-mpl to compare
+    return fig
+
+def test_plot_transect_size(glider_data):
+    # Generate the plot with a specific size
+    fig, ax = plt.subplots(figsize=(15, 9))
+    glider_data.plot_transect(var="temperature", ax=ax)
+    assert fig.get_size_inches() == pytest.approx([15., 9.])
